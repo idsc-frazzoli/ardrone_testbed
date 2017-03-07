78461638d13a9df3011840bbbfa957568f8f5cdc
Unknown <lojonas@ethz.ch>
2017-03-07 16:59:53 +0100

    usleep libary include
    #include <unistd.h> to be included so it compiles on my computer

--- ORB_SLAM2/src/LocalMapping.cc
+++ ORB_SLAM2/src/LocalMapping.cc
@@ -1,3 +1,5 @@
+// modified to include  #include <unistd.h> from J. Londschien
+
 /**
 * This file is part of ORB-SLAM2.
 *
@@ -24,6 +26,7 @@
 #include "Optimizer.h"
 
 #include<mutex>
+#include <unistd.h>
 
 namespace ORB_SLAM2
 {
