8ad650541c89722586779e7e3bbefdfd0bac66ea
Daniel Gehrig <daniel.gehrig1@gmx.ch>
2017-03-08 15:31:55 +0100

    WIP  bug in CMakeLists of orb
    Migrating from tf to tf2 in ros_mono.cc

--- ORB_SLAM2/Examples/ROS/ORB_SLAM2/src/ros_mono.cc
+++ ORB_SLAM2/Examples/ROS/ORB_SLAM2/src/ros_mono.cc
@@ -23,231 +23,188 @@
 #include<algorithm>
 #include<fstream>
 #include<chrono>
-#include <math.h> 
+#include <math.h>
 
 #include <ros/ros.h>
 
 #include <sensor_msgs/PointCloud.h>
 #include <cv_bridge/cv_bridge.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
-#include <tf/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Vector3.h>
+#include <tf2/convert.h>
+#include <tf2/LinearMath/Transform.h>
 
 #include<opencv2/core/core.hpp>
 
 #include"../../../include/System.h"
 #include <../../opt/ros/kinetic/include/geometry_msgs/QuaternionStamped.h>
-#include <tf/transform_broadcaster.h>
-#include <tf/transform_listener.h>
-
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2/convert.h>
 
 using namespace std;
 
 class ImageGrabber
 {
 public:
-    ImageGrabber(ORB_SLAM2::System* pSLAM):mpSLAM(pSLAM), pc(){
-        pc.header.frame_id= "/first_keyframe";
+    ImageGrabber ( ORB_SLAM2::System* pSLAM ) :mpSLAM ( pSLAM ), pc() {
+        pc.header.frame_id= "/orb/odom";
+        pose_out_.header.frame_id = "/orb/odom";
     }
-    void GrabImage(const sensor_msgs::ImageConstPtr& msg);
-    
+    void GrabImage ( const sensor_msgs::ImageConstPtr& msg );
+
     ORB_SLAM2::System* mpSLAM;
-    
-    sensor_msgs::PointCloud pc;
+
     bool initialized = false;
+
+    sensor_msgs::PointCloud pc;
     geometry_msgs::PoseWithCovarianceStamped pose_out_;
-    tf::Quaternion quat_cam_drone;
-    tf::Quaternion tfqt_adj;
-    
-    //tf::StampedTransform world_to_level_transform;
-    tf::StampedTransform world_to_first_keyframe_transform;
-    tf::StampedTransform first_keyframe_to_ardrone_base_frontcam_transform;
-    tf::TransformListener tf_listener;
+
+    tf2::Transform orb_odom_to_frontcam, world_to_orb_odom, frontcam_to_base_link;
+    tf2_ros::Buffer tf_buffer;
 };
 
-int main(int argc, char **argv)
+int main ( int argc, char **argv )
 {
-    ros::init(argc, argv, "Mono");
+    ros::init(argc,argv,"Mono");
     ros::start();
-    
-    if(argc != 3)
-    {
-        cerr << endl << "Usage: rosrun ORB_SLAM2 Mono path_to_vocabulary path_to_settings" << endl;        
+
+    if ( argc != 3 ) {
+        cerr << endl << "Usage: rosrun ORB_SLAM2 Mono path_to_vocabulary path_to_settings" << endl;
         ros::shutdown();
         return 1;
-    }    
-    
+    }
+
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
-    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,true);
-    
-    ImageGrabber igb(&SLAM);
-    
+    ORB_SLAM2::System SLAM ( argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,true );
+
+    ImageGrabber igb ( &SLAM );
+
     ros::NodeHandle nodeHandler;
-    ros::Subscriber sub = nodeHandler.subscribe("/camera/image_raw", 1, &ImageGrabber::GrabImage,&igb);
-    ros::Publisher pub = nodeHandler.advertise<sensor_msgs::PointCloud>("/orb/point_cloud", 2);
-    ros::Publisher pose_pub = nodeHandler.advertise<geometry_msgs::PoseWithCovarianceStamped>("/orb/pose",2);
-    
-    ros::Rate loop_rate(30);
-    
-    while (ros::ok()) {    
+    ros::Subscriber sub = nodeHandler.subscribe ( "/camera/image_raw", 1, &ImageGrabber::GrabImage,&igb );
+    ros::Publisher point_cloud_pub = nodeHandler.advertise<sensor_msgs::PointCloud> ( "/orb/point_cloud", 2 );
+    ros::Publisher pose_pub = nodeHandler.advertise<geometry_msgs::PoseWithCovarianceStamped> ( "/orb/pose",2 );
+
+    ros::Rate loop_rate ( 30 );
+
+    while ( ros::ok() ) {
         ros::spinOnce();
-        pub.publish(igb.pc);
-	if (igb.initialized) pose_pub.publish(igb.pose_out_);
+        point_cloud_pub.publish ( igb.pc );
+        if ( igb.initialized ) {
+            pose_pub.publish ( igb.pose_out_ );
+        }
         loop_rate.sleep();
     }
-    
-    
+
+
     // Stop all threads
     SLAM.Shutdown();
-    
+
     // Save camera trajectory
-    SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
-    
+    SLAM.SaveKeyFrameTrajectoryTUM ( "KeyFrameTrajectory.txt" );
+
     ros::shutdown();
-    
+
     return 0;
 }
 
 //callback
-void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr& msg)
+void ImageGrabber::GrabImage ( const sensor_msgs::ImageConstPtr& msg )
 {
-    static tf::TransformBroadcaster br;
-    tf::Transform transform;
     // Copy the ros image message to cv::Mat.
     cv_bridge::CvImageConstPtr cv_ptr;
-    try
-    {
-        cv_ptr = cv_bridge::toCvShare(msg);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-        ROS_ERROR("cv_bridge exception: %s", e.what());
+    try {
+        cv_ptr = cv_bridge::toCvShare ( msg );
+    } catch ( cv_bridge::Exception& e ) {
+        ROS_ERROR ( "cv_bridge exception: %s", e.what() );
         return;
     }
-    //     Main slam routine. Extracts new pose
-    cv::Mat pose = mpSLAM->TrackMonocular(cv_ptr->image,cv_ptr->header.stamp.toSec());
-    
-    // if points can be tracked then broadcast the pose 
-    if (not pose.empty()) {
-	if (not initialized) {
-	  
-	// listen to transform odom baselink for trafo from world to first_keyframe
-	//tf_listener.lookupTransform("/odom", "/ardrone_base_link", ros::Time(0), world_to_level_transform); // orientation of drone wrt north & down
-	tf_listener.lookupTransform("/ardrone_base_link", "/ardrone_base_frontcam", ros::Time(0), world_to_first_keyframe_transform); // orientation of camera wrt drone
-	world_to_first_keyframe_transform.setOrigin(tf::Vector3(0, 0, 0));
+    // Main slam routine. Extracts new pose
+    cv::Mat pose = mpSLAM->TrackMonocular ( cv_ptr->image,cv_ptr->header.stamp.toSec() );
 
-	initialized = true;
-    }//ardrone_base_frontcam
-        
+    // if points can be tracked then broadcast the pose
+    if ( not pose.empty() ) {
+        if ( not initialized ) {
+            tf2::convert ( tf_buffer.lookupTransform ( "/odom", "/ardrone_base_frontcam", ros::Time ( 0 ) ), world_to_orb_odom );
+            tf2::convert ( tf_buffer.lookupTransform ( "/adrone_base_frontcam", "/adrone_base_link", ros::Time ( 0 ) ), frontcam_to_base_link );
+
+            initialized = true;
+        }
+
+        static tf2_ros::TransformBroadcaster br;
+
         // set transformation between first_keyframe and ardrone_base_frontcam
-        tf::Vector3 origin;
-        tf::Quaternion transform_quat;
-        tf::Matrix3x3 transform_matrix;
-	
-        origin.setValue(pose.at<float>(0,3), pose.at<float>(1,3), pose.at<float>(2,3));
-       
-	transform_matrix.setValue(pose.at<float>(0,0), pose.at<float>(0,1), pose.at<float>(0,2), 
-		                  pose.at<float>(1,0), pose.at<float>(1,1), pose.at<float>(1,2), 
-                                  pose.at<float>(2,0), pose.at<float>(2,1), pose.at<float>(2,2));
-	
-        transform_matrix.getRotation(transform_quat); 
-        
-        first_keyframe_to_ardrone_base_frontcam_transform.setOrigin(transform_matrix.transpose() * origin * -1);
-        first_keyframe_to_ardrone_base_frontcam_transform.setRotation(transform_quat.inverse());
-	 
-	// broadcast all transforms
-	ros::Time t = ros::Time::now();
-	//br.sendTransform(tf::StampedTransform(world_to_level_transform.inverse(), t, "/level", "/world"));// world = odom at time 0, level = base_link at time 0 (when orb finds keyframe)
-	br.sendTransform(tf::StampedTransform(world_to_first_keyframe_transform.inverse(), t, "/first_keyframe", "/world"));
-	br.sendTransform(tf::StampedTransform(first_keyframe_to_ardrone_base_frontcam_transform.inverse(), t, "/ardrone_base_frontcam", "/first_keyframe"));
-	
-	// plot pose odom to world
-	tf::Transform pose_out_tf_non_stamped;
-	tf::StampedTransform ardrone_base_frontcam_to_ardrone_base_link;
-	
-	tf_listener.lookupTransform("/ardrone_base_frontcam", "/ardrone_base_link", ros::Time(0), ardrone_base_frontcam_to_ardrone_base_link); // orientation of odom wrt world
-	
-	pose_out_tf_non_stamped = first_keyframe_to_ardrone_base_frontcam_transform;// * ardrone_base_frontcam_to_ardrone_base_link;
-	tf::StampedTransform pose_out_tf = tf::StampedTransform(pose_out_tf_non_stamped, t, "/world", "/ardrone_base_link");
-	
-	// 	tf::TransformListener listener1;
+        tf2::Vector3 translation = tf2::Vector3 ( pose.at<float> ( 0,3 ), pose.at<float> ( 1,3 ), pose.at<float> ( 2,3 ) );
+        tf2::Matrix3x3 rotation = tf2::Matrix3x3 ( pose.at<float> ( 0,0 ), pose.at<float> ( 0,1 ), pose.at<float> ( 0,2 ),
+                                  pose.at<float> ( 1,0 ), pose.at<float> ( 1,1 ), pose.at<float> ( 1,2 ),
+                                  pose.at<float> ( 2,0 ), pose.at<float> ( 2,1 ), pose.at<float> ( 2,2 ) );
 
-// 	listener1.lookupTransform("/world", "/ardrone_base_link", ros::Time(0), pose_out_tf); // orientation of odom wrt world
-	pose_out_.header.frame_id = "/first_keyframe";
-	pose_out_.header.stamp = t;
-	
-	tf::Quaternion pose_orientation = pose_out_tf.getRotation();
-	tf::Vector3 pose_origin = pose_out_tf.getOrigin();
-	pose_out_.pose.pose.orientation.x = pose_orientation.getX(); 
-	pose_out_.pose.pose.orientation.y = pose_orientation.getY();
-	pose_out_.pose.pose.orientation.z = pose_orientation.getZ();
-	pose_out_.pose.pose.orientation.w = pose_orientation.getW();
-	pose_out_.pose.pose.position.x = pose_origin.getX();
-	pose_out_.pose.pose.position.y = pose_origin.getY();
-	pose_out_.pose.pose.position.z = pose_origin.getZ();
-	
-	
-	//tf::StampedTransform transform_out = tf::StampedTransform(transform, t, "/level", "/camera");
-	
-	//br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "/level", "/camera"));
-	
-	// rotate camera pose into world frame
-	
-	
-	// define helper quaternions
-// 	tf::Quaternion origin_transform = tf::Quaternion(-0.5, 0.5, -0.5, 0.5);
-// 	tf::Quaternion other_transform = tf::Quaternion(0.5, -0.5, 0.5, 0.5);
-// 	tf::Quaternion rotation_transform = tf::Quaternion(0, -sqrt(2)/2, 0, sqrt(2)/2);
-// 	
-// 	rotation_transform = other_transform.inverse() * transform_quat.inverse() * rotation_transform;
-// 	
-// 	tf::Vector3 origin_i_rotated = tf::quatRotate(origin_transform, transform_matrix.transpose() * origin * -1);
-// 	
-// 	pose_out_.pose.pose.orientation.x = rotation_transform.getX();
-// 	pose_out_.pose.pose.orientation.y = rotation_transform.getY();
-// 	pose_out_.pose.pose.orientation.z = rotation_transform.getZ();
-// 	pose_out_.pose.pose.orientation.w = rotation_transform.getW();
-// 	pose_out_.pose.pose.position.x = origin_i_rotated.getX();
-// 	pose_out_.pose.pose.position.y = origin_i_rotated.getY();
-// 	pose_out_.pose.pose.position.z = origin_i_rotated.getZ();
-// 	
-	//TODO: Set covariance
-	for(auto& x:pose_out_.pose.covariance)
-	  x = 0.0;
-	
-	pose_out_.pose.covariance[0] = 1;
-	pose_out_.pose.covariance[7] = 1;
-	pose_out_.pose.covariance[14] = 1;
-	pose_out_.pose.covariance[21] = 1;
-	pose_out_.pose.covariance[28] = 1;
-	pose_out_.pose.covariance[35] = 1;
-	
-// 	pose_out.pose.covariance. = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-// 				    0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-// 				    0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-// 				    0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-// 				    0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-// 				    0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
-// 				//    pose_out.pose.cova
+        orb_odom_to_frontcam = tf2::Transform ( rotation.transpose(), rotation.transpose() * translation * -1);
+
+        // broadcast all transforms
+        geometry_msgs::TransformStamped T_wo, T_of, T_fb;
+
+        T_wo.transform = tf2::toMsg ( world_to_orb_odom );
+        T_of.transform = tf2::toMsg ( orb_odom_to_frontcam );
+        T_fb.transform = tf2::toMsg ( frontcam_to_base_link );
+
+        T_wo.header.stamp = T_of.header.stamp = T_fb.header.stamp = ros::Time::now();
+
+        T_wo.child_frame_id = T_of.header.frame_id = "/orb/odom";
+        T_of.child_frame_id = T_fb.header.frame_id = "/orb/frontcam";
+        T_fb.child_frame_id = "/orb/base_link";
+        T_wo.header.frame_id = "/world";
+
+        br.sendTransform ( T_wo );
+        br.sendTransform ( T_of );
+        br.sendTransform ( T_fb );
+
+        // get pose of base_link in world frame
+        tf2::Transform T_wb = world_to_orb_odom * orb_odom_to_frontcam * frontcam_to_base_link;
+
+        // set pose
+        pose_out_.header.stamp = ros::Time::now();
+
+        pose_out_.pose.pose.orientation = tf2::toMsg ( T_wb.getRotation() );
+        pose_out_.pose.pose.position.x = T_wb.getOrigin().getX();
+        pose_out_.pose.pose.position.y= T_wb.getOrigin().getY();
+        pose_out_.pose.pose.position.z = T_wb.getOrigin().getZ();
+
+        //TODO: Set covariance
+        for ( auto& x:pose_out_.pose.covariance ) {
+            x = 0.0;
+        }
+
+        pose_out_.pose.covariance[0] = 1;
+        pose_out_.pose.covariance[7] = 1;
+        pose_out_.pose.covariance[14] = 1;
+        pose_out_.pose.covariance[21] = 1;
+        pose_out_.pose.covariance[28] = 1;
+        pose_out_.pose.covariance[35] = 1;
     }
-    
+
     // gets points from most recent frame
     // gets all points
     const std::vector<ORB_SLAM2::MapPoint*> &point_cloud = mpSLAM->mpMap->GetAllMapPoints();
     // TODO: make efficient (use mpSLAM->GetTrackedMapPoints() to get most recent points)
     pc.points.clear();
-    for(size_t i=0; i<point_cloud.size();i++)
-    {
-        if(point_cloud[i]->isBad()/* or spRefMPs.count(vpMPs[i])*/)
+    for ( size_t i=0; i<point_cloud.size(); i++ ) {
+        if ( point_cloud[i]->isBad() /* or spRefMPs.count(vpMPs[i])*/ ) {
             continue;
+        }
         cv::Mat pos = point_cloud[i]->GetWorldPos();
         geometry_msgs::Point32 pp;
-        pp.x=pos.at<float>(0);
-        pp.y=pos.at<float>(1);
-        pp.z=pos.at<float>(2);
-        
-        pc.points.push_back(pp);
+        pp.x=pos.at<float> ( 0 );
+        pp.y=pos.at<float> ( 1 );
+        pp.z=pos.at<float> ( 2 );
+
+        pc.points.push_back ( pp );
     }
-    
+
 }
 
 
