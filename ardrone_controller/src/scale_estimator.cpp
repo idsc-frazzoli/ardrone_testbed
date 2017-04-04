7933a3a6d0e933eee3d20403a7224e49db6cb603
Max <goettgem@student.ethz.ch>
2017-04-05 01:00:15 +0200

    compiling and working - own implementation of path for the pose - kicked out hector from project - first estimate for covariances of scaled pose - some medium size fixes in orb_adapter - rviz bugs fixed such that pose and path are now shown correctly without any crahses

--- ardrone_controller/src/scale_estimator.cpp
+++ ardrone_controller/src/scale_estimator.cpp
@@ -2,9 +2,10 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
-#include <ardrone_autonomy/Navdata.h>
-
+#include <nav_msgs/Path.h>
 #include <std_msgs/Float32.h>
+
+#include <ardrone_autonomy/Navdata.h>
 
 #include <tf/LinearMath/Transform.h>
 #include <tf/transform_listener.h>
@@ -18,11 +19,12 @@ class ScaleEstimator {
     float scale = 1; // has units m^-1
     bool isStarted = false;
 
+    tf::Vector3 curr_orb_position;
     tf::Vector3 filt_position;
     tf::Quaternion orb_orientation;
     tf::Quaternion yaw_correction_quat;
-    tf::StampedTransform yaw_correction_before_jump_tf;
-    tf::Vector3 curr_orb_position;
+    tf::StampedTransform yaw_before_jump_tf;
+    tf::TransformListener tf_listener;
 
     vector<double> scale_vector;
     vector<double> orbAverages;
@@ -30,10 +32,11 @@ class ScaleEstimator {
 
     vector<ardrone_autonomy::Navdata> nav_data_queue;
     vector<geometry_msgs::PoseWithCovarianceStamped> orb_data_queue;
-    tf::TransformListener tf_listener;
+    vector<geometry_msgs::PoseStamped> poses;
 
     geometry_msgs::PoseWithCovarianceStamped newest_orb_msg;
     geometry_msgs::PoseWithCovarianceStamped filt_pose;
+    nav_msgs::Path scaled_path;
 
     double get_average_z ( vector<ardrone_autonomy::Navdata> &queue ) {
         double data = 0;
@@ -112,7 +115,7 @@ class ScaleEstimator {
         orb_orientation.setZ ( newest_orb_msg.pose.pose.orientation.z );
         orb_orientation.setW ( newest_orb_msg.pose.pose.orientation.w );
 
-        orb_orientation =  orb_orientation * yaw_correction_quat;
+        orb_orientation =  orb_orientation * yaw_correction_quat; // TODO: Something still seems to be wrong with the yaw correction - we have to figure out exactly how to to the tf's and double check the implementation
 
         // orientation
         filt_pose.pose.pose.orientation.x = orb_orientation.getX();
@@ -123,16 +126,51 @@ class ScaleEstimator {
         // position
         filt_pose.pose.pose.position.x = newest_orb_msg.pose.pose.position.x / scale;
         filt_pose.pose.pose.position.y = newest_orb_msg.pose.pose.position.y / scale;
-        filt_pose.pose.pose.position.z = newest_orb_msg.pose.pose.position.z / scale;
+        filt_pose.pose.pose.position.z = newest_orb_msg.pose.pose.position.z / scale; //TODO: Test if it is better to use the altd message for altitude
 
         for ( auto & x : filt_pose.pose.covariance ) {
             x = 0.0;
         }
+        filt_pose.pose.covariance[0] = newest_orb_msg.pose.covariance[0] / scale;
+        filt_pose.pose.covariance[7] = newest_orb_msg.pose.covariance[7] / scale;
+        filt_pose.pose.covariance[14] = newest_orb_msg.pose.covariance[14] / scale;
+        filt_pose.pose.covariance[21] = newest_orb_msg.pose.covariance[21] / scale;
+        filt_pose.pose.covariance[28] = newest_orb_msg.pose.covariance[28] / scale;
+        filt_pose.pose.covariance[35] = newest_orb_msg.pose.covariance[35] / scale;
 
         cout << "SCALED POSITION: " << endl
         << "X: " << filt_pose.pose.pose.position.x << endl
         << "Y: " << filt_pose.pose.pose.position.y << endl 		//debug
         << "Z: " << filt_pose.pose.pose.position.z << endl;
+
+
+        geometry_msgs::PoseStamped path_pose;
+        path_pose.header.stamp = ros::Time::now();
+        path_pose.header.frame_id = "odom";
+
+        // orientation
+        path_pose.pose.orientation.x = filt_pose.pose.pose.orientation.x;
+        path_pose.pose.orientation.y = filt_pose.pose.pose.orientation.y;
+        path_pose.pose.orientation.z = filt_pose.pose.pose.orientation.z;
+        path_pose.pose.orientation.w = filt_pose.pose.pose.orientation.w;
+
+        // position
+        path_pose.pose.position.x = newest_orb_msg.pose.pose.position.x / scale;
+        path_pose.pose.position.y = newest_orb_msg.pose.pose.position.y / scale;
+        path_pose.pose.position.z = newest_orb_msg.pose.pose.position.z / scale; //TODO: Test if it is better to use the altd message for altitude
+        //TODO: Implement something that takes the nav_data.altd message if it is available (drone at maximum height of 4m) or elsewise orb.z / scale but calculate the offset in the z coordinate (after takeoff) anyway!!
+
+        poses.push_back ( path_pose );
+    }
+
+    void update_path_array () {
+        geometry_msgs::PoseStamped pose_array[poses.size()];
+        for ( int i = 0; i < poses.size() ; i++ ) {
+            pose_array[i] = poses.at ( i );
+        }
+        scaled_path.header.frame_id = "odom";
+        scaled_path.header.stamp = ros::Time::now();
+        scaled_path.poses = poses;
     }
 
     void orb_callback ( geometry_msgs::PoseWithCovarianceStamped msg ) {
@@ -141,27 +179,26 @@ class ScaleEstimator {
     void nav_callback ( ardrone_autonomy::Navdata msg ) {
         if ( isStarted ) {
             nav_data_queue.push_back ( msg );
-
             return;   // we already have the yaw error correction so this function is very slim after initial yaw correction
         }
         if ( msg.altd > 0 && not isStarted ) { //the yaw jump occurs exactly when the first altd message arrives + make sure it gets called only once
             isStarted = true;
 
-            tf::StampedTransform yaw_correction_after_jump_tf;
+            tf::StampedTransform yaw_after_jump_tf;
             try {
-                tf_listener.lookupTransform ( "/ardrone_base_link", "odom", ros::Time ( 0 ), yaw_correction_after_jump_tf );
+                tf_listener.lookupTransform ( "/ardrone_base_link", "odom", ros::Time ( 0 ), yaw_after_jump_tf );
             } catch ( tf::LookupException e ) {
                 ROS_WARN ( "Failed to lookup transformation after yaw jump! Do not trust the direction of flight!" );
 
             }
 
-            tf::Transform yaw_correction_transform = yaw_correction_before_jump_tf * yaw_correction_after_jump_tf;
+            tf::Transform yaw_correction_transform = yaw_before_jump_tf * yaw_after_jump_tf;
             yaw_correction_quat = yaw_correction_transform.getRotation();
 
             return;
         }
         try {
-            tf_listener.lookupTransform ( "odom", "/ardrone_base_link", ros::Time ( 0 ), yaw_correction_before_jump_tf ); //we keep updating yaw_correction_before_jump_tf as long as there is no jump
+            tf_listener.lookupTransform ( "odom", "/ardrone_base_link", ros::Time ( 0 ), yaw_before_jump_tf ); //we keep updating yaw_correction_before_jump_tf as long as there is no jump
         }                                                                                                             //to make sure we have the latest coordinate frame before the jump
         catch ( tf::LookupException e ) {
             cout << "failed to lookup ardrone_base_link - is the driver running?" << endl;
@@ -186,6 +223,7 @@ int main ( int argc, char **argv ) {
     // publish scale and filtered orb pose
     ros::Publisher filt_orb_pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped> ( "/ardrone/pose_scaled",2 );
     ros::Publisher orb_scale_pub = nh.advertise<std_msgs::Float32> ( "/orb/scale", 2 );
+    ros::Publisher path_pub = nh.advertise<nav_msgs::Path> ( "/ardrone/path",2 );
 
     int rate = 60;
     ros::Rate loop_rate ( rate );
@@ -212,6 +250,8 @@ int main ( int argc, char **argv ) {
 
                 orb_scale_pub.publish ( scale_est.scale );
                 filt_orb_pub.publish ( scale_est.filt_pose );
+                scale_est.update_path_array();
+                path_pub.publish ( scale_est.scaled_path );
 
                 scale_est.orbAverages.clear();
                 scale_est.altAverages.clear();
